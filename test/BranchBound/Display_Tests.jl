e755c7eae50b58ce0947edea45964aa7fae61a7a
Wilhelm <maw16110@ad.uconn.edu>
2018-04-20 12:12:44 -0400

    Update tests again...

--- test/BranchBound/Display_Tests.jl
+++ test/BranchBound/Display_Tests.jl
@@ -8,11 +8,8 @@ using EAGO
 S = BnBSolver()
 S.Verbosity = "Full"
 
-println("test #1:")
 @test_nowarn EAGO.print_node!(S,1,3.0,[Interval(1.0,2.0)])
-println("test #2:")
 @test_nowarn EAGO.print_results!(S,2.0,[1.5],false,true)
-println("test #3:")
 @test_nowarn EAGO.print_results!(S,2.0,[1.5],false,false)
 
 end
