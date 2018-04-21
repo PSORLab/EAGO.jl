8cc054d69a13913d7c2e6118765a056cc42aace2
Unknown <wilhelmme@gmail.com>
2018-04-21 09:38:30 -0400

    Win32 Fix

--- src/src/McCormick/utils/root_finding.jl
+++ src/src/McCormick/utils/root_finding.jl
@@ -91,8 +91,8 @@ are the function `f` evaluated at `a`,`b`, and `c` respectively. The inputs
 `envp1` and `envp2` are the envelope calculation parameters. The value `init` is
 the iteration number of the golden section method.
 """
-@inline function golden_section_it(init::Int64,a::T,fa::T,b::T,fb::T,c::T,
-                                   fc::T,f::Function,envp1::T,envp2::T) where {T<:Real}
+@inline function golden_section_it(init::Q,a::T,fa::T,b::T,fb::T,c::T,
+                                   fc::T,f::Function,envp1::T,envp2::T) where {Q<:Integer,T<:Real}
   b_t_x::Bool = (c-b > b-a)
   if (b_t_x)
     x::T = b + (2.0-golden)*(c-b)
