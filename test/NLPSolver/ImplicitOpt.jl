05901537b57ddaaa00c76904e1ba2fee329e5847
Unknown <wilhelmme@gmail.com>
2018-05-05 10:49:46 -0400

    Test Update

--- test/NLPSolver/ImplicitOpt.jl
+++ test/NLPSolver/ImplicitOpt.jl
@@ -59,6 +59,47 @@ sol1b = getsolution(internalmodel(jm1b))
 @test isapprox(sol1b[2],9.00,atol=1E-3)
 @test isapprox(getobjval(internalmodel(jm1b)),-8.531128966881054,atol=1E-3)
 
+jm1c = Model(solver=EAGO_NLPSolver(LBD_func_relax = "NS-STD-OFF",
+                                   LBDsolvertype = "LP",
+                                   probe_depth = -1,
+                                   variable_depth = 1000,
+                                   DAG_depth = -1,
+                                   STD_RR_depth = 1000,
+                                   ImplicitFlag = true,
+                                   verbosity = "Normal",
+                                   validated = true))
+xc = @variable(jm1c, [i=1:2], lowerbound=LBD1a_func(i), upperbound=UBD1a_func(i))
+@NLconstraint(jm1c, xc[1]^2 + xc[2]*xc[1] + 4 == 0.0 )
+@NLobjective(jm1c, Min, xc[1])
+status1c = Solve_Implicit(jm1c,f1,h1,hj1,x->[],1)
+
+
+jm1d = Model(solver=EAGO_NLPSolver(LBD_func_relax = "NS-STD-OFF",
+                                   LBDsolvertype = "LP",
+                                   probe_depth = -1,
+                                   variable_depth = 1000,
+                                   DAG_depth = -1,
+                                   STD_RR_depth = 1000,
+                                   ImplicitFlag = true,
+                                   verbosity = "Normal",
+                                   validated = true))
+xd = @variable(jm1d, [i=1:2], lowerbound=LBD1b_func(i), upperbound=UBD1b_func(i))
+@NLconstraint(jm1d, xd[1]^2 + xd[2]*xd[1] + 4 == 0.0 )
+@NLobjective(jm1d, Min, xd[1])
+status1d = Solve_Implicit(jm1d,f1,h1,hj1,x->[],1)
+
+
+sol1c = getsolution(internalmodel(jm1a))
+@test status1c == :Optimal
+@test isapprox(sol1c[1],-0.7639320302435194,atol=1E-3)
+@test isapprox(sol1c[2],6.00,atol=1E-3)
+@test isapprox(getobjval(internalmodel(jm1c)),-0.7639320302435194,atol=1E-3)
+
+sol1d = getsolution(internalmodel(jm1d))
+@test status1d == :Optimal
+@test isapprox(sol1d[1],-8.531128966881054,atol=1E-3)
+@test isapprox(sol1d[2],9.00,atol=1E-3)
+@test isapprox(getobjval(internalmodel(jm1d)),-8.531128966881054,atol=1E-3)
 
 end
 end
