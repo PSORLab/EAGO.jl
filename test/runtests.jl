7f33c9fbc4b4e617ce0663d9756e97c6da5a479c
Matthew Wilhelm <wilhelmme@gmail.com>
2019-11-05 08:40:07 -0500

    Update Examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,9 @@ using IntervalArithmetic
 const MOI = MathOptInterface
 
 include("branch_bound.jl")
-#include("ParametricInterval/runtests.jl")
 include("mccormick.jl")
 include("domain_reduction.jl")
 #include("relaxations.jl")
 include("optimizer.jl")
-#include("script_optimizer.jl")
+include("script_optimizer.jl")
 #include("semiinfinite.jl")
