6a61e07e9d606e9391d4e1652ca5d3215cec5807
Matt W <matthew.wilhelm@uconn.edu>
2021-01-25 16:14:47 -0500

    Change License to MIT

--- src/eago_optimizer/functions/nonlinear/reverse_pass.jl
+++ src/eago_optimizer/functions/nonlinear/reverse_pass.jl
@@ -1,8 +1,5 @@
 # Copyright (c) 2018: Matthew Wilhelm & Matthew Stuber.
-# This work is licensed under the Creative Commons Attribution-NonCommercial-
-# ShareAlike 4.0 International License. To view a copy of this license, visit
-# http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative
-# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+# This code is licensed under MIT license (see LICENSE.md for full details)
 #############################################################################
 # EAGO
 # A development environment for robust and global optimization
