e413f9789238cd6384a0fadba853187494bf24ef
Matt W <matthew.wilhelm@uconn.edu>
2021-06-25 03:19:04 -0400

    Work on subgradient refinement of udfs

--- src/eago_optimizer/functions/nonlinear/composite_relax/forward_user_function.jl
+++ src/eago_optimizer/functions/nonlinear/composite_relax/forward_user_function.jl
@@ -1,13 +1,49 @@
 #=
 # TODO: Infer dimensionality of user function from context type
-struct RelaxMeta{T} end
-const F_RELAX_CTX{T} = RelaxCtx{RelaxMeta{T}}
+struct RelaxMeta{N,T} 
+    p::SVector{N,T}
+    pL::SVector{N,T}
+    pU::SVector{N,T}
+    v::VariableValues{Float64}
+    s::Vector{Int}
+    ϵ::Float64
+    subgradient_refinement::Bool
+end
+
+@context RelaxCtx
+
+function ctx_subgradient_refine(c::RelaxCtx{RelaxMeta{N,T},Nothing}, z::MC{N,T}) where {N,T}
+    m = ctx.metadata
+    v = ctx.metadata.v
+    s = ctx.metadata.s
+    ϵ = ctx.metadata.ϵ
+    return m.subgradient_refinement ? set_value_post(z, m.v, m.s, m.ϵ), z
+end
 
 for k in UNIVARIATE_ATOM_TYPES
     f = UNIVARIATE_ATOM_DICT[k]
-    eval(quote Cassette.overdub(F_RELAX_CTX{T}, ::typeof($f), x::T) where {T<:Number} = $f(x) end)
+    @eval function Cassette.overdub(ctx::RelaxCtx{RelaxMeta{N,T},Nothing}, ::typeof($f), x::MC{N,T}) where {N,T}
+        return ctx_subgradient_refine(ctx, ($f)(x))
+    end
 end
-Cassette.overdub(c::F_RELAX_CTX{T}, ::typeof($f), x::T) where {T<:Number} = $f(x)
+
+for k in BIVARIATE_ATOM_TYPES
+    f = BIVARIATE_ATOM_DICT[k]
+    @eval function Cassette.overdub(ctx::RelaxCtx{RelaxMeta{N,T},Nothing}, ::typeof($f), x::MC{N,T}, y::NumberNotRelax) where {N,T}
+        return ctx_subgradient_refine(ctx, ($f)(x, y))
+    end
+    @eval function Cassette.overdub(ctx::RelaxCtx{RelaxMeta{N,T},Nothing}, ::typeof($f), y::NumberNotRelax, x::MC{N,T}) where {N,T}
+        return ctx_subgradient_refine(ctx, ($f)(y, x))
+    end
+end
+
+#=
+for k in NARITY_ATOM_TYPES
+    f = NARITY_ATOM_DICT[k]
+    @eval function Cassette.overdub(ctx::RelaxCtx{RelaxMeta{N,T},Nothing}, ::typeof($f), ...) where {N,T}
+    end
+end
+=#
 
 # commonly used storage objects automatically get promoted to use the set-valued storage
 Cassette.overdub(::F_RELAX_CTX{T}, ::typeof(zeros), n::Int) where {T<:Number} = zeros(T, n)
