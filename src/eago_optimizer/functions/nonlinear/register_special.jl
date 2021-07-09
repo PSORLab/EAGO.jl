14b65302c2afe25782ff8174657a1a70e3b55e31
Matt W <matthew.wilhelm@uconn.edu>
2021-07-09 15:38:38 -0400

    Rename from swish1 to swish

--- src/eago_optimizer/functions/nonlinear/register_special.jl
+++ src/eago_optimizer/functions/nonlinear/register_special.jl
@@ -29,7 +29,7 @@ function register_eago_operators!(m::JuMP.Model)
     JuMP.register(m, :bisigmoid, 1, bisigmoid, McCormick.bisigmoid_deriv, McCormick.bisigmoid_deriv2)
     JuMP.register(m, :softsign, 1, softsign, McCormick.softsign_deriv, McCormick.softsign_deriv2)
     JuMP.register(m, :gelu, 1, gelu, McCormick.gelu_deriv, McCormick.gelu_deriv2)
-    JuMP.register(m, :swish1, 1, swish1, McCormick.swish1_deriv, McCormick.swish1_deriv2)
+    JuMP.register(m, :swish1, 1, swish, McCormick.swish_deriv, McCormick.swish_deriv2)
     JuMP.register(m, :xabsx, 1, xabsx, McCormick.xabsx_deriv, McCormick.xabsx_deriv2)
 
     # register activatio functions w/ parameters
