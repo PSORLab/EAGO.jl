0f02da74e7bac93ad86e93b838b5569732e0a872
Dimitri Alston <dimitri.alston@uconn.edu>
2023-06-09 16:32:11 -0400

    Update moit_tests.jl
    Removed these tests too soon..

--- test/moit_tests.jl
+++ test/moit_tests.jl
@@ -103,7 +103,13 @@ function test_runtests()
                                 "test_cpsat_CountAtLeast",
                                 "test_cpsat_Table",
                                 "test_linear_Semicontinuous_integration",
-                                "test_linear_Semiinteger_integration"
+                                "test_linear_Semiinteger_integration",
+
+                                # Remove these tests after MOI 1.17.2
+                                "test_objective_ObjectiveSense_in_ListOfModelAttributesSet",
+                                "test_objective_ScalarAffineFunction_in_ListOfModelAttributesSet",
+                                "test_objective_ScalarQuadraticFunction_in_ListOfModelAttributesSet",
+                                "test_objective_VariableIndex_in_ListOfModelAttributesSet"
 
                                 ],
                       exclude_tests_after = v"0.10.5")
